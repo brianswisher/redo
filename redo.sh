2fef2447a8b0c709f9e18f655a275eadc8e8af4d
brianswisher <bswisher@gmail.com>
2015-10-30 07:52:55 -0700

    finalize script

--- redo.sh
+++ redo.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 
 redo() {
-  echo ":${BRANCH}_clone:"
-  # git checkout `pbpaste`
-
-  # git checkout master
-  # git fetch origin
-  # git pull
-  # git checkout `pbpaste`
-  # git rebase master
-  # git push origin `pbpaste` -f
-  # git checkout master
-  # git merge --squash `pbpaste`
+  git checkout ${BRANCH}
+  git branch "${BRANCH}_clone"
+  git checkout master
+  git branch -D ${BRANCH}
+  git checkout -b ${BRANCH}
+  git merge --squash "${BRANCH}_clone"
 }
 
 redo
