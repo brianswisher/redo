eebd25d015d613316b3b1fe5cb2de211fa2cbd6e
brianswisher <bswisher@gmail.com>
2015-10-30 07:32:10 -0700

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # redo
+
+1. `cd directory_name` (github repos)
+2. `git branch`
+3. select `branch_name` & `command + c` (copy) to the clipboard
+4. paste this into a terminal:
+
+```bash
+curl -s https://raw.githubusercontent.com/brianswisher/shipit/master/redo.sh | bash
+```
---
+++ redo.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+redo() {
+  git checkout `pbpaste`
+  # git checkout master
+  # git fetch origin
+  # git pull
+  # git checkout `pbpaste`
+  # git rebase master
+  # git push origin `pbpaste` -f
+  # git checkout master
+  # git merge --squash `pbpaste`
+}
+
+redo
